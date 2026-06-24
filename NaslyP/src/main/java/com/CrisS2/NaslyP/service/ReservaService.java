39a6d847398413bbb84f0360045a352d21da5984
devstiven12 <devstiven12@gmail.com>
2026-06-24 18:37:02 -0500

    se crea archivo de service

--- NaslyP/src/main/java/com/CrisS2/NaslyP/service/ReservaService.java
+++ NaslyP/src/main/java/com/CrisS2/NaslyP/service/ReservaService.java
@@ -1,5 +1,21 @@
 package com.CrisS2.NaslyP.service;
 
+import com.CrisS2.NaslyP.strategy.CalculoTarifaStrategy;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservaService {
-    
+
+    private CalculoTarifaStrategy strategy;
+
+    public void setStrategy(CalculoTarifaStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public double executeStrategy(double precioBase, int noches) {
+        if (this.strategy == null) {
+            throw new IllegalStateException("No se ha seleccionado ninguna estrategia de tarifa.");
+        }
+        return this.strategy.calcularTotal(precioBase, noches);
+    }
 }
