46c8a2e5703510f4a70561e38795216495101563
devstiven12 <devstiven12@gmail.com>
2026-06-24 17:44:55 -0500

    se crea la estructura del taller

---
+++ NaslyP/src/main/java/com/CrisS2/NaslyP/NaslyPApplication.java
@@ -0,0 +1,13 @@
+package com.CrisS2.NaslyP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaslyPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaslyPApplication.class, args);
+	}
+
+}
---
+++ NaslyP/src/test/java/com/CrisS2/NaslyP/NaslyPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CrisS2.NaslyP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaslyPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
